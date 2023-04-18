93d57576cb02d1e2372c427d676ca560a4dcd8a2
sparrowsl <benthorpe.dev@gmail.com>
2023-04-18 14:59:23 +0000

    chore: install mdi icon package

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import Features from "./components/Features.jsx";
 import Header from "./components/Header.jsx";
 
 function App() {
-	return <Header />;
+	return (
+		<>
+			<Header />
+			<Features />
+		</>
+	);
 }
 
 export default App;
